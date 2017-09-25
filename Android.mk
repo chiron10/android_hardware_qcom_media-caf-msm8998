b170fcb0fa641d09be09ddc390a83882a0f5e13b
maxwen <max.weninger@gmail.com>
2017-09-25 17:16:31 +0200

    Add TARGET_QCOM_MEDIA_VARIANT
    Change-Id: I21010298b0fc3863445d0f40c53197ffcff2ae75
    Signed-off-by: RahifM <faizel326@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8996)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
-#Compile these for all targets under QCOM_BOARD_PLATFORMS list.
+#Compile these for all targets under QCOM_BOARD_PLATFORMS list
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
@@ -20,4 +21,5 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 include $(QCOM_MEDIA_ROOT)/libsidebandstreamhandle/Android.mk
 endif
 
+endif
 endif
